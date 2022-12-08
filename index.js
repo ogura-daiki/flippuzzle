b57453cfd68a8580609554e558c1cc249e386fed
OGURA DAIKI <dkog2001@gmail.com>
2022-12-09 00:52:03 +0900

    クリック判定を変更、指が動いても同じパネル内で指を離せば良いことにした

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ class FlipBoard extends LitElement {
       height:var(--panel-width, var(--panel-width-def));
       background-size: 100% auto;
       image-rendering: pixelated;
+      user-select:none;
     }
     .panel.front{
       animation: flip var(--flip-duration, var(--flip-duration-def)) steps(calc(var(--img-count) - 1)) 1 reverse forwards;
@@ -85,15 +86,21 @@ class FlipBoard extends LitElement {
   #panel({x,y}){
     const flipped = this.board[y][x];
     return html`
-    <div class="panel ${flipped?"back":"front"}" data-x=${x} data-y=${y}></div>
+    <div
+      class="panel ${flipped?"back":"front"}"
+      data-x=${x} data-y=${y}
+      ></div>
     `;
   }
-  #onClick(e){
+  #getPanelPos(e){
     const classNames = new Set(e.target.className.split(" ").filter(v=>v));
     if(!classNames.has("panel")) return;
     let {x, y} = e.target.dataset;
     x=+x;
     y=+y;
+    return {x,y};
+  }
+  #onClick({x,y}){
     for(let iy = clamp(0, y-1, 4); iy < Math.min(y+2, 4); iy+=1){
       for(let ix = clamp(0, x-1, 4); ix < Math.min(x+2, 4); ix+=1){
         this.board[iy][ix] = !this.board[iy][ix];
@@ -102,9 +109,55 @@ class FlipBoard extends LitElement {
     sound.flip.play();
     this.requestUpdate();
   }
+  #equalsPos(p1, p2){
+    return p1.x === p2.x && p1.y === p2.y;
+  }
+  #pos = null;
+  #beforeTouch = null;
   render(){
     return html`
-    <div class="container" @click=${e=>this.#onClick(e)}>
+    <div class="container"
+      @pointerdown=${e=>{
+        this.#pos = this.#getPanelPos(e);
+      }}
+      @touchmove=${e=>{
+        this.#beforeTouch = e.touches[0];
+      }}
+      @touchend=${e=>{
+        console.log(e.type);
+        if(!this.#pos) return;
+        if(!this.#beforeTouch) return;
+        const {pageX:x, pageY:y} = this.#beforeTouch;
+        const panels = [...this.renderRoot.querySelectorAll(".panel")];
+        const target = panels.find(p=>{
+          const rect = p.getBoundingClientRect();
+          return clamp(rect.left, x, rect.right) === x &&
+            clamp(rect.top, y, rect.bottom) === y;
+        });
+        console.log(target);
+        const newPos = {x:+target.dataset.x, y:+target.dataset.y};
+        if(this.#equalsPos(this.#pos, newPos)){
+          this.#onClick(this.#pos);
+          this.#pos=null;
+        }
+      }}
+      @mouseup=${e=>{
+        console.log(e.type);
+        if(!this.#pos) return;
+        const {pageX:x, pageY:y} = e;
+        const panels = [...this.renderRoot.querySelectorAll(".panel")];
+        const target = panels.find(p=>{
+          const rect = p.getBoundingClientRect();
+          return clamp(rect.left, x, rect.right) === x &&
+            clamp(rect.top, y, rect.bottom) === y;
+        });
+        const newPos = {x:+target.dataset.x, y:+target.dataset.y}
+        if(this.#equalsPos(this.#pos, newPos)){
+          this.#onClick(this.#pos);
+          this.#pos=null;
+        }
+      }}
+    >
       ${range(4).map(y=>html`${
         range(4).map(x=>html`${
           this.#panel({x,y})
@@ -135,13 +188,12 @@ class App extends LitElement{
       box-sizing:border-box;
       display:flex;
       flex-flow:column nowrap;
-      padding:8px;
+      padding:16px;
       gap:8px;
     }
     .holder{
       flex-grow:1;
       position:relative;
-      margin:8px;
     }
     flip-board{
       position:absolute;
