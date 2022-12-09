17bd6e29ca44cc4526879ef5b1b5a4c4531197a7
OGURA DAIKI <dkog2001@gmail.com>
2022-12-10 00:21:28 +0900

    開発環境以外で開発者メニューが開かれることを妨害する

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 import "./js/element/FlipBoard.js";
 import QuestionPage from "./js/page/Question.js";
 
+if(!isLocalhost){
+  //開発者メニューが開かれることを妨害する
+  document.addEventListener('keydown',(e) => {
+    if (e.key === "F12" || (e.ctrlKey && e.key==="I")) { // F12キー
+      e.preventDefault();
+    }
+  });
+  document.addEventListener('contextmenu',(e) => {
+    e.preventDefault();
+  });
+}
+
 document.body.append(new QuestionPage());
